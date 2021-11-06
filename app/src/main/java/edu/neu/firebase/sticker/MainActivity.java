c201b25abe0a50e56dbff903b810b7e77f2af5ce
Caroline Fu <34133445+yfu6@users.noreply.github.com>
2021-11-06 16:22:08 -0700

    Update MainActivity.java
    Log in

--- app/src/main/java/edu/neu/firebase/sticker/MainActivity.java
+++ app/src/main/java/edu/neu/firebase/sticker/MainActivity.java
@@ -2,15 +2,32 @@ package edu.neu.firebase.sticker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String SERVER_KEY = "AAAAHSvPDy4:APA91bHRckV19Q1vMipF5JNDQJV9--eWIHQ_5q3S4gOQWaCggx5J-nMVK3JdUmuftyvwisetrjh7WdIgVeVNX257OMro2XOwRPR_nBsWXb6FmIKEu1KnfIA2CJeb8uT_dJrpwtiP3VaD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button button1 = (Button) findViewById(R.id.button_login);
+        button1.setOnClickListener(v -> {
+        System.out.println("Login button clicked");
+        TextView usernameTextView = (TextView) findViewById(R.id.editUsername);
+        String username = usernameTextView.getText().toString().replaceAll("\\s", "");
+
+        if (username.length() == 0) {
+            Toast.makeText(MainActivity.this, "Please enter user Name", Toast.LENGTH_SHORT).show();
+        }else {
+            Intent intent1 = new Intent(MainActivity.this, HomePageActivity.class);
+            intent1.putExtra("USERNAME", username);
+            startActivity(intent1);
+        }
+    });
+}}
