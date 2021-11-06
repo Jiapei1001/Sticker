55b0f0f2402e195e8af24b16b32967307dadbb4a
Yu Fu <fu.yu4@northeastern.edu>
2021-11-06 16:13:08 -0700

    Yu FU

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 # Sticker
-Assignment 7 for Mobile Development Course
-
-Team members:
-By Yongzheng Qi, Qiangqiang Zhang, Yu Fu, Jiapei Li
-Northeastern University, Silicon Valley
-
-# Log in Screen
--- app/src/main/java/edu/neu/firebase/sticker/HomePageActivity.java
+++ app/src/main/java/edu/neu/firebase/sticker/HomePageActivity.java
@@ -2,7 +2,11 @@ package edu.neu.firebase.sticker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +14,6 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
     }
 }
