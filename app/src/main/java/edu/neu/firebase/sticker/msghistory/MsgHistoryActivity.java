a6031e13e221b03b84f486005d7476f84077f855
Jiapei1001 <jiapeili@live.com>
2021-11-06 15:59:42 -0700

    add msg history layout && add msgCard class

---
+++ app/src/main/java/edu/neu/firebase/sticker/msghistory/MsgHistoryActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.firebase.sticker.msghistory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.neu.firebase.sticker.R;
+
+public class MsgHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msg_history);
+    }
+}
