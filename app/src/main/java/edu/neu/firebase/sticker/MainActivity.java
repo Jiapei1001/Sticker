404a33250810fb45b312b004ad384ee1471c06b0
Jiapei1001 <jiapeili@live.com>
2021-11-06 12:13:35 -0700

    update gradle to include required packages

--- app/src/main/java/edu/neu/firebase/sticker/MainActivity.java
+++ app/src/main/java/edu/neu/firebase/sticker/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String SERVER_KEY = "	AAAApB74PQw:APA91bHZihIOA8gKyTeC9jhfNS1HJ7b5UmwfAGgp5pfs33Dv_-FjwuMkppSeWeYboN1biVV-Neb0K5slGHuND8IN-fpM6BcQUrDBWfA3OZ68aqAWa_pcSD76OTI0Lq84yvYL27JRjNS3";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
