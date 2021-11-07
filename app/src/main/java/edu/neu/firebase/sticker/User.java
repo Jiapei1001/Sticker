5d92490a6897db11630aca181b43a5d59e68f039
Caroline Fu <34133445+yfu6@users.noreply.github.com>
2021-11-06 17:15:34 -0700

    Add files via upload

---
+++ app/src/main/java/edu/neu/firebase/sticker/User.java
@@ -0,0 +1,14 @@
+package edu.neu.firebase.sticker;
+
+public class User {
+    public String username;
+    public int userid;
+
+    public User(String username, int userid) {
+        this.username = username;
+        this.userid = userid;
+    }
+
+
+
+}
