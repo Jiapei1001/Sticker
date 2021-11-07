e42c6e3fbb69ea2a7e2ea0d6c1befaa2f7016c0b
Jiapei1001 <jiapeili@live.com>
2021-11-06 22:52:02 -0700

    finalize msgHistory

--- app/src/main/java/edu/neu/firebase/sticker/MainActivity.java
+++ app/src/main/java/edu/neu/firebase/sticker/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String SERVER_KEY = "AAAAHSvPDy4:APA91bHRckV19Q1vMipF5JNDQJV9--eWIHQ_5q3S4gOQWaCggx5J-nMVK3JdUmuftyvwisetrjh7WdIgVeVNX257OMro2XOwRPR_nBsWXb6FmIKEu1KnfIA2CJeb8uT_dJrpwtiP3VaD";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/edu/neu/firebase/sticker/msghistory/MsgHistoryActivity.java
+++ app/src/main/java/edu/neu/firebase/sticker/msghistory/MsgHistoryActivity.java
@@ -1,15 +1,22 @@
 package edu.neu.firebase.sticker.msghistory;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import edu.neu.firebase.sticker.R;
 
@@ -37,6 +44,32 @@ public class MsgHistoryActivity extends AppCompatActivity {
         msgHistoryAdapterView.setLayoutManager(layoutManager);
         msgHistoryAdapterView.setAdapter(msgHistoryAdapter);
 
+        // connect to firebase db
+        DatabaseReference fireDB = FirebaseDatabase.getInstance().getReference();
 
+        // set event listener
+        fireDB.child("messageHistory").addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // clean up
+                        msgCardList.clear();
+                        // update
+                        for (DataSnapshot ds : snapshot.getChildren()) {
+                            MsgCard msgCard = ds.getValue(MsgCard.class);
+                            msgCardList.add(msgCard);
+                        }
+                        Collections.sort(msgCardList, (a, b) -> a.getSendTime().compareTo(b.getSendTime()));
+
+                        // update msgHistoryAdapter as msg contents have changed
+                        msgHistoryAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                }
+        );
     }
 }
