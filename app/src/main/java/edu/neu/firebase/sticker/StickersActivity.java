8a0776bd9fb65ca31d69ea72b721f25fa7bb4fe9
yzqi <yongqi014@gmail.com>
2021-11-06 21:41:29 -0700

    create new yongzheng branch

---
+++ app/src/main/java/edu/neu/firebase/sticker/StickersActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.firebase.sticker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StickersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stickers);
+    }
+}
