b5d84318ca1d7ba59c1fc0e3fa12cd7faebb105f
yzqi <yongqi014@gmail.com>
2021-11-07 02:46:45 -0800

    update send messageinfo to firebase

--- app/src/main/java/edu/neu/firebase/sticker/StickersActivity.java
+++ app/src/main/java/edu/neu/firebase/sticker/StickersActivity.java
@@ -1,19 +1,27 @@
 package edu.neu.firebase.sticker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StickersActivity extends AppCompatActivity {
     private String sender;
     private String receiver;
-    private TextView text;
+    private EditText inputText;
     private DatabaseReference database;
-    private DatabaseReference messageInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +29,9 @@ public class StickersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stickers);
         sender = getIntent().getExtras().get("sender").toString();
         receiver = getIntent().getExtras().get("receiver").toString();
-        text = (TextView)findViewById(R.id.text);
-
+        inputText = (EditText)findViewById(R.id.text);
     }
+
     public void onClickButtonSticker(View view) {
         int stickerId = view.getId();
         String time = String.valueOf(System.currentTimeMillis());
@@ -31,6 +39,28 @@ public class StickersActivity extends AppCompatActivity {
     }
 
     private void uploadMessageInfo(String sender, String receiver, String time, int stickerId) {
+        String messageRef = "user " + sender + " send sticker " + stickerId + " to " + " user " + receiver + " at " + time;
+        database = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference messageInfo = database.child("massages").push();
+        String pushId = messageInfo.getKey();
+        Map messageBody = new HashMap<>();
+        messageBody.put("sticker id", stickerId);
+        messageBody.put("from", sender);
+        messageBody.put("to", receiver);
+        messageBody.put("time", time);
 
+        Map messageDatails = new HashMap<>();
+        messageDatails.put(messageRef + "/" + pushId, messageBody);
+        messageInfo.updateChildren(messageDatails).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(StickersActivity.this, "Sticker sent successfully!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(StickersActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                }
+                inputText.setText("");
+            }
+        });
     }
 }
