e9f001e7b398c297e9081109e32b2c1a811e9f65
Jiapei1001 <jiapeili@live.com>
2021-11-07 18:55:23 -0800

    add sticker history

---
+++ app/src/main/java/edu/neu/firebase/sticker/stickerHistory/ReceivedStickersActivity.java
@@ -0,0 +1,73 @@
+package edu.neu.firebase.sticker.stickerHistory;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import edu.neu.firebase.sticker.R;
+import edu.neu.firebase.sticker.Utils;
+
+public class ReceivedStickersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_received_stickers);
+
+        Intent intent = getIntent();
+        String currUser = intent.getStringExtra("current_user_username");
+        Button button = (Button) findViewById(R.id.show_received_stickers_btn);
+        TextView textView = (TextView) findViewById(R.id.received_stickers_texts);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // connect to DB
+                DatabaseReference fireDB = FirebaseDatabase.getInstance().getReference();
+                fireDB.child("messageHistory").orderByKey().addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Map<String, Integer> stickerCnt = new HashMap<>();
+                        for (DataSnapshot kv : snapshot.getChildren()) {
+                            if (kv.child("sender").getValue(String.class).equals(currUser)) {
+                                String sticker = kv.child("sticker").getValue(String.class);
+                                stickerCnt.put(sticker, stickerCnt.getOrDefault(sticker, 1) + 1);
+                            }
+                        }
+                        List<Map.Entry<String, Integer>> list = new LinkedList<>(stickerCnt.entrySet());
+                        Collections.sort(list, (a, b) -> b.getValue().compareTo(a.getValue()));
+
+                        String result = "\n\n";
+                        for (Map.Entry<String, Integer> e : list) {
+                            result += e.getKey() + " has been sent: " + e.getValue() + " times\n\n";
+                        }
+                        textView.setText(result);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+    }
+}
